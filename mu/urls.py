b255e734c16cc7a4bc099bfee3dffd6a6356086e
dan <dan@dog.(none)>
2013-10-12 16:05:01 -0700

    Adding the lib and the urls

---
+++ mu/urls.py
@@ -0,0 +1,22 @@
+import settings
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+PROJECT_PATH = settings.PROJECT_PATH
+STATIC_PATH = settings.PROJECT_PATH + "/"
+
+urlpatterns = patterns('',
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+   (r'^home/', 'mu.suggest.views.home'), 
+   (r'^search/', 'mu.suggest.views.autosuggest'),
+
+   (r'^css/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_PATH +'html/css/'}),
+   (r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_PATH + 'html/js/'}),
+   (r'^html/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_PATH + 'html/templates/'}),
+   (r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_PATH + 'html/images/'}),
+
+)
